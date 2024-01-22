ea1e3d2a35d7442545b06d298ea52abf9acfd809
Lucas <lucasoliveirars22@gmail.com>
2024-01-21 23:58:20 -0300

    atualizando cards de tasks automaticamente

--- ToDoList/App.tsx
+++ ToDoList/App.tsx
@@ -6,26 +6,50 @@ import PendingTasks from './src/components/pendingTasksDashboard';
 import SearchBar from './src/components/searchBar';
 import Task from './src/components/task';
 import Title from './src/components/title';
+import { useState } from 'react';
 
+const tasksList = [
+  { title: 'Treino', subtitle: 'Braços' },
+  { title: 'Festa', subtitle: 'Carnaval' },
+  { title: 'Treino', subtitle: 'Pernas' },
+];
 
 export default function App() {
+  const [completedTasks, setCompletedTasks] = useState(0);
+  const [pendingTasks, setPendingTasks] = useState(tasksList.length);
+
+  const updateTaskCount = (isChecked: boolean) => {
+    if (isChecked) {
+      setCompletedTasks((prevCount) => prevCount + 1);
+      setPendingTasks((prevCount) => Math.max(0, prevCount - 1)); 
+    } else {
+      setCompletedTasks((prevCount) => Math.max(0, prevCount - 1));
+      setPendingTasks((prevCount) => prevCount + 1);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='inverted' />
       <Title name="Dashboard" fontSize={28}/>
-      <TaskProgressBoard numDone={1} totalTasks={2}/>
+      <TaskProgressBoard numDone={completedTasks} totalTasks={completedTasks + pendingTasks}/>
       <View style={styles.taskStatus}>
-        <CompletedTasks num={1}/>
-        <PendingTasks num={2}/>
+        <CompletedTasks num={completedTasks}/>
+        <PendingTasks num={pendingTasks}/>
       </View>
       <SearchBar />
-      <Task title="Treino" subtitle="Subtítulo" active={false} />
-      <Task title="Festa" subtitle="Subtítulo" active={true} />
-      <Task title="Ensaio" subtitle="Subtítulo" active={true} />
+      {tasksList.map((task, index) => (
+        <Task
+          key={index}
+          title={task.title}
+          subtitle={task.subtitle}
+          active={false}
+          onToggle={updateTaskCount}
+        />
+      ))}
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
